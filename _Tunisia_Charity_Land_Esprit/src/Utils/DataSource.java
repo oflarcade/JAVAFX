c3634a3fc273ca638fdac048f450cdcdd6ca68d9
omar farouk <omarfarouk.lakhdar@esprit.tn>
2018-10-18 16:51:36 +0100

    Initial Commit

---
+++ _Tunisia_Charity_Land_Esprit/src/Utils/DataSource.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author oflcad
+ */
+public class DataSource {
+    private static DataSource instance;
+    private Connection connection;
+    private String urlString = "jdbc:mysql://localhost:3306/esprit";
+    private String username = "root";
+    private String password = "";
+
+    private DataSource() throws SQLException {
+        try {
+            //Class.forName("com.mysql.jdbc.Driver");
+            this.connection = DriverManager.getConnection(urlString, username, password);
+        } catch (SQLException ex) {
+            System.out.println("Database Connection Creation Failed : " + ex.getMessage());
+        }
+    }
+    
+    
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static DataSource getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DataSource();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new DataSource();
+        }
+
+        return instance;
+    }
+}
+
